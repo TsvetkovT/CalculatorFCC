e92364693816604a5ed2567ca7f062a36d364855
TsvetkovT <tsvetelin.p.tsvetkov@gmail.com>
2018-01-12 13:16:57 +0200

    calculator logic done

--- calculator.js
+++ calculator.js
@@ -2,7 +2,19 @@
  * Created by TT on 5.01.18 г..
  */
 
-/** Truncate text **/
+/** Define variables **/
+var arrFormula = [];
+var result = '';
+var equation = '';
+var pointIndex = 0; //defines the position of the decimal point in arrFormula array
+//var currentStr = $("#mod").text();
+var specButtons = ['/', '*', '-', '+', '='];
+var point = ['.'];
+
+/** ========================= **/
+
+
+/** Truncate text function **/
 
 function truncateText(selector, maxLength) {
     var element = document.querySelector(selector),
@@ -14,26 +26,35 @@ function truncateText(selector, maxLength) {
     return truncated;
 }
 
+/** ========================= **/
+
+/** Round result function **/
+function roundResult(arr) {
+    var rightDecimal1 = 0; //defines the number of numbers after decimal point for left value in equation (arrFotmula[0])
+    var rightDecimal2 = 0; //defines the number of numbers after decimal point for right value in equation (number of array element after '.')
+
+    rightDecimal1 = arr[0].substring(arr[0].indexOf(".") + 1);
+    rightDecimal1 = rightDecimal1.length;
+
+    return rightDecimal1;
+
+}
 
 
+/** ========================= **/
 
 function reply_click(clicked_id) {
     return clicked_id;
 }
 
-var arrFormula = [];
+
 
 $(".black").each(function (index) {
     $(this).on("click", function () {
 
         // For the key value
         var key = $(this).attr('value');
-        var result = '';
-        var equation = '';
-        var pointIndex = 0;
-        var currentStr = $("#mod").text();
-        var specButtons = ['/', '*', '-', '+', '='];
-        var point = ['.'];
+
 
 
 
@@ -54,6 +75,7 @@ $(".black").each(function (index) {
             } else {
                 arrFormula.push(key);
                 console.log(arrFormula);
+                //roundResult(arrFormula);
                 break;
 
 
@@ -77,9 +99,12 @@ $(".black").each(function (index) {
             arrFormula.push(key);
             equation = arrFormula.join('');
             result = eval(equation.slice(0, -1));
+            result = result.toString();
+            var precision = result.substring(result.indexOf(".") + 1).length;
+
 
             arrFormula = [];
-            arrFormula.push(result.toString(),key);
+            arrFormula.push(result.toPrecision(precision),key);
 
 
                 //$("#mod").text(equation.slice(0, -1))
@@ -101,21 +126,6 @@ $(".black").each(function (index) {
 
         }
 
-
-
-
-        // if (key == '.') {
-        //
-        //     if (arrFormula[arrFormula.length - 1] !== '.') {
-        //         //arrFormula.pop();
-        //         arrFormula.push(key);
-        //         //arrFormula.pop();
-        //     } else if (arrFormula[arrFormula.length - 1] == '.') {
-        //         arrFormula.pop();
-        //         arrFormula.push(key);
-        //        // arrFormula.pop();
-        //     }
-        // }
 
     });
 });
